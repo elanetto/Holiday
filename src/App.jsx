293cafe6a65a0ea405cea4bb9b6c311c67df5ae2
elanetto <101054274+elanetto@users.noreply.github.com>
2025-04-08 19:04:38 +0200

    initial setup

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import './index.css'
+
+function App() {
+
+  return (
+    <>
+      <div className='flex flex-col items-center justify-center text-center bg-pink-200 h-screen'>
+        <h1 className='text-5xl font-bold text-pink-500'>Holidaze</h1>
+        <p className='text-gray-600'>...coming soon</p>
+      </div>
+    </>
+  )
+}
+
+export default App
