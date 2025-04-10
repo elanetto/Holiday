17324dd24fdb9693513a683bc0102304b117191a
elanetto <101054274+elanetto@users.noreply.github.com>
2025-04-10 12:19:20 +0200

    Added an intitial layout and path to get started

---
+++ src/components/Layout/Footer/index.jsx
@@ -0,0 +1,7 @@
+export function Footer () {
+    return (
+        <footer className="bg-pink-950 text-white h-80 w-full flex justify-center items-center text-center">
+            <p>This is a footer</p>
+        </footer>
+    )
+}
