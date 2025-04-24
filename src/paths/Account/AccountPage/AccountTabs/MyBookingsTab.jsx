c861415102f881cfb5f96d06dd212380a0f0d063
elanetto <101054274+elanetto@users.noreply.github.com>
2025-04-24 09:27:19 +0100

    Created MyBookings component and added it to Account page

--- src/paths/Account/AccountPage/AccountTabs/MyBookingsTab.jsx
+++ src/paths/Account/AccountPage/AccountTabs/MyBookingsTab.jsx
@@ -1,9 +1,12 @@
+import MyBookings from "../../../../components/Booking/MyBookings";
+
 const MyBookingsTab = () => {
 
     return (
         <div className="p-8 text-center w-full">
             <h1>My Bookings</h1>
             <p>This is where you can see your bookings.</p>
+            <MyBookings />
         </div>
     );
 };
